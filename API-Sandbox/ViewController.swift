3ff584f22718e599d730f033351f458e3cad6d53
Isabella Garcia-Camargo <isabellagc2010@gmail.com>
2017-08-31 19:29:31 -0700

    initial commit

---
+++ API-Sandbox/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  API-Sandbox
+//
+//  Created by Dion Larson on 6/24/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+import AlamofireImage
+import AlamofireNetworkActivityIndicator
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var rightsOwnerLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    var currentMovie: Movie? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        exerciseOne()
+//        exerciseTwo()
+//        exerciseThree()
+        
+        let apiToContact = "https://itunes.apple.com/us/rss/topmovies/limit=25/json"
+        // This code will call the iTunes top 25 movies endpoint listed above
+        Alamofire.request(apiToContact).validate().responseJSON() { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    
+                    // Do what you need to with JSON here!
+                    // The rest is all boiler plate code you'll use for API requests
+                    // Enter SwiftyJSON!
+                    // moviesData now contains a JSON object representing all the data in the JSON file.
+                    // This JSON file contains the same data as the tutorial example.
+                    
+                    // We've done you the favor of grabbing an array of JSON objects representing each movie
+                    let allMovies = json["feed"]["entry"].arrayValue
+                    let randomMovie = Movie(json: allMovies[0])
+                    self.currentMovie = randomMovie
+                    
+                    self.movieTitleLabel.text = randomMovie.name
+                    self.rightsOwnerLabel.text = randomMovie.rightsOwner
+                    self.releaseDateLabel.text = randomMovie.releaseDate
+                    self.priceLabel.text = randomMovie.price.description
+                    self.loadPoster(urlString: randomMovie.imageLink)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Updates the image view when passed a url string
+    func loadPoster(urlString: String) {
+        posterImageView.af_setImage(withURL: URL(string: urlString)!)
+    }
+    
+    @IBAction func viewOniTunesPressed(_ sender: AnyObject) {
+        UIApplication.shared.openURL(URL(string: (currentMovie?.link)!)!)
+    }
+    
+}
+
